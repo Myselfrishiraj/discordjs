2acba0dabe80469802c2e293850f6938b9401a37
Myselfrishiraj <88775015+Myselfrishiraj@users.noreply.github.com>
2022-06-17 15:24:18 +0530

    Update tdm.js

--- src/Commands/tdm.js
+++ src/Commands/tdm.js
@@ -1,1 +1,5 @@
-if (message.content === 'tdm'){ (await message.guild.members.fetch()).each(member => { // Looping through each member of the guild. // Trying to send a message to the member. // This method might fail because of the member's privacy settings, so we're using .catch member.send("test").catch(e => console.error(`Couldn't DM member ${member.user.tag}`)); }); }
+message.guild.members.cache.forEach(member => { // Looping through each member of the guild.
+    // Trying to send a message to the member.
+    // This method might fail because of the member's privacy settings, so we're using .catch
+    member.send("Hello, this is my message!").catch(e => console.error(`Couldn't DM member ${member.user.tag}`));
+});
